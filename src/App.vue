bb6cd12fd03fd34f5b323160c5dd365f45530a79
Erik Figueiredo <erik.figueiredo@gmail.com>
2019-06-04 10:26:24 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <layout>
+      <template v-slot:footer>
+        <p>Ir para o site da School of  Net</p>
+      </template>
+
+      <template v-slot:header>
+        <img src="./assets/logo.png">
+      </template>
+
+      <e-btn href="https://schoolofnet.com" target="_blank">
+        <i class="material-icons">school</i>
+        {{ name }}
+      </e-btn>
+    </layout>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import EBtn from './components/button'
+import layout from './components/layout'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: 'School of Net',
+    };
+  },
+  components: {
+    HelloWorld,
+    'e-btn': EBtn,
+    layout
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,22 @@
+<template>
+    <a class="e-btn">
+        <slot></slot>
+    </a>
+</template>
+<style>
+.e-btn {
+  display: inline-block;
+  padding: 10px 15px;
+  background: #e74c3c;
+  color: white;
+  text-decoration: none;
+  border-radius: 6px;
+  border-right: 2px #2c3e50 solid;
+  border-bottom: 2px #2c3e50 solid;
+  transition: background ease .5s;
+}
+
+.e-btn:hover {
+  background: #c0392b;
+}
+</style>
